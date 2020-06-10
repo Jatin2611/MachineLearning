2942cf2392d4700974ea95446ee10d09f84826e7
jatin2611 <2017.jatin.bhagchandani@ves.ac.in>
2020-06-10 21:16:11 +0530

    Simple Linear Regression

---
+++ Part-2 Simple Linear Regression/my_simple_linear_regression.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  9 20:36:29 2020
+
+@author: Jatin
+"""
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0) 
