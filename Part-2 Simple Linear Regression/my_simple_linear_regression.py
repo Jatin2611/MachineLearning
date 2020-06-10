c6dba4f8d3193fb534236a4a8097c097ed11033d
jatin2611 <2017.jatin.bhagchandani@ves.ac.in>
2020-06-10 22:21:08 +0530

    SLR commit

--- Part-2 Simple Linear Regression/my_simple_linear_regression.py
+++ Part-2 Simple Linear Regression/my_simple_linear_regression.py
@@ -17,3 +17,19 @@ y = dataset.iloc[:, 1].values
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0) 
+
+#Fitting Simple linear regression to Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#Predicting the test set results
+y_pred = regressor.predict(X_test)
+
+#Visualizing test results
+plt.scatter(X_test,y_test,color="red")
+plt.plot(X_train, regressor.predict(X_train), color="blue")
+plt.title("Experience vs Salary")
+plt.xlabel("Experience")
+plt.ylabel("Salary")
+plt.show()
